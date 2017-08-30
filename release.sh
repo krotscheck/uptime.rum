5d50066a33777a9e0b3b82b3b7b0c3117540ce60
Michael Krotscheck <krotscheck@gmail.com>
2017-08-30 14:31:56 -0700

    Package rum.js into an NPM package
    This project checks for a new uptime.com/rum.js file, and if found
    will tag and release a new version of this package. It is intended
    to be used in concert with travis' hourly cron job.

---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -x
+
+# Export github credentials
+export GIT_SSH_COMMAND='ssh -i ./uptime_github_deploy_key'
+chmod 0600 ./uptime_github_deploy_key
+
+# Setup github ID
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+git remote add origin-ssh git@github.com:krotscheck/uptime.rum.git
+
+# wget the latest, only if changed
+wget -N https://uptime.com/static/rum/compiled/rum.js
+
+git diff-index --quiet HEAD --
+
+if [ $? -ne 0 ]
+then
+    git add .
+    npm version -f minor
+    git push origin-ssh HEAD:master
+    git push origin-ssh --tags
+else
+    echo "No changes required, exiting."
+fi
